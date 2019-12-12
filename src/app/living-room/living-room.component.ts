b3040d93c27fdf42be30493bb2f5aac9329a3af9
DawaT <tshering0096@gmail.com>
2019-12-12 10:11:29 +0600

    ui fixes

--- src/app/living-room/living-room.component.ts
+++ src/app/living-room/living-room.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-living-room',
-  templateUrl: './living-room.component.html',
-  styleUrls: ['./living-room.component.scss']
+    selector: 'app-living-room',
+    templateUrl: './living-room.component.html',
+    styleUrls: ['./living-room.component.scss']
 })
 export class LivingRoomComponent implements OnInit {
+    ac = 'OFF';
+    light = 'ON';
+    lamp = 'OFF';
+    window = 'OPEN';
+    lamp1 = 'OFF';
 
-  constructor() { }
+    setAC(e) {
+        if (e.checked) {
+            this.ac = 'ON';
+        } else {
+            this.ac = 'OFF';
+        }
+    }
 
-  ngOnInit() {
+    setLight(e) {
+        if (e.checked) {
+            this.light = 'ON';
+        } else {
+            this.light = 'OFF';
+        }
+    }
+
+    setLamp(e) {
+        if (e.checked) {
+            this.lamp = 'ON';
+        } else {
+            this.lamp = 'OFF';
+        }
+    }
+
+    setWindow(e) {
+        if (e.checked) {
+            this.window = 'OPEN';
+        } else {
+            this.window = 'CLOSED';
+        }
+    }
+
+  setLamp1(e) {
+    if (e.checked) {
+      this.lamp1 = 'ON';
+    } else {
+      this.lamp1 = 'OFF';
+    }
   }
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -12,6 +12,7 @@ export class LoginPageComponent implements OnInit {
 
   username: string;
   password: string;
+  hide = true ;
 
   ngOnInit() {
   }
